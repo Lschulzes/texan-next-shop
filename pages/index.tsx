2ca05cf02ee5f7ea4f8d5e8d4b6ddf5516282cff
lschulzes <lucassilvaiee@gmail.com>
2022-07-05 11:39:49 -0300

    feat: add basic products to front pagee

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,15 @@
-import { Typography } from "@mui/material";
+import {
+  Card,
+  CardActionArea,
+  CardMedia,
+  Grid,
+  Typography,
+} from "@mui/material";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import Layout from "../components/Layout";
+import { initialData } from "../database/products";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
@@ -17,6 +24,22 @@ const Home: NextPage = () => {
       <Typography variant="h2" mb="1">
         All Products
       </Typography>
+
+      <Grid container spacing={4}>
+        {initialData.products.map(({ slug, images, title }) => (
+          <Grid item xs={6} sm={4} key={slug}>
+            <Card>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  image={`products/${images[0]}`}
+                  alt={title}
+                />
+              </CardActionArea>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
     </Layout>
   );
 };
