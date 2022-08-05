15c519a7895a509d435110e6c052b2c7cf96c52d
lschulzes <lucassilvaiee@gmail.com>
2022-08-05 12:10:02 -0300

    feat: listen for stripe events, and on successful payment, update order's isPaid status

--- pages/api/webhooks.ts
+++ pages/api/webhooks.ts
@@ -1,6 +1,8 @@
 import { buffer } from 'micro';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import Stripe from 'stripe';
+import { db } from '../../database';
+import { OrderModel } from './../../models/OrderModel';
 
 export default async function webhookHandler(req: NextApiRequest, res: NextApiResponse) {
   switch (req.method) {
@@ -19,12 +21,32 @@ const validateStripe = async (req: NextApiRequest, res: NextApiResponse) => {
   if (!sig || !webhookSecret || !stripeSecret) return;
   const stripe = new Stripe(stripeSecret, { typescript: true, apiVersion: '2022-08-01' });
   try {
-    stripe.webhooks.constructEvent(buf, sig, webhookSecret);
+    const event = stripe.webhooks.constructEvent(buf, sig, webhookSecret);
+    await handleEvent(event as unknown as StripeEvent);
+
     return res.status(200).send('success');
   } catch (error) {
     console.error(`Webhook error: ${error}`);
     return res.status(400).send(error);
   }
+};
+
+const handleEvent = async (event: StripeEvent) => {
+  if (event.data.object.object === 'charge') {
+    if (event.data.object.paid !== true) return;
+    await db.connect();
+    await OrderModel.updateOne({ transactionId: event.data.object.payment_intent }, { $set: { isPaid: true } });
+  }
+};
+
+type StripeEvent = {
+  data: {
+    object: {
+      object: 'charge' | 'payment_intent';
+      paid: boolean;
+      payment_intent: string;
+    };
+  };
 };
 
 export const config = {
