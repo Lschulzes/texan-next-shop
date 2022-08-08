3396e45ef7a5931326ed66f4b2a46e866d429283
lschulzes <lucassilvaiee@gmail.com>
2022-08-07 21:09:54 -0300

    feat: add admin order id view

---
+++ pages/admin/orders/[id].tsx
@@ -0,0 +1,122 @@
+import { CreditCardOffOutlined, CreditCardOutlined } from '@mui/icons-material';
+import { Box, Card, CardContent, Chip, Divider, Grid, Link, Typography } from '@mui/material';
+import { GetServerSideProps } from 'next';
+import { getSession } from 'next-auth/react';
+import NextLink from 'next/link';
+import { useRouter } from 'next/router';
+import CartList from '../../../components/Cart/CartList';
+import OrderSummary from '../../../components/Cart/OrderSummary';
+import Layout from '../../../components/Layout';
+import { IOrder } from '../../../interfaces';
+import { getOrderByID } from '../../api/orders/[id]';
+
+type OrderPageProps = { order: IOrder };
+
+const OrderPage = ({ order }: OrderPageProps) => {
+  const router = useRouter();
+  const { id } = router.query;
+
+  const { address, country, lastName, name, phoneNumber } = order.billingAddress;
+  const { discount, total, subTotal, isPaid } = order;
+  const quantity = order.orderItems.length;
+
+  return (
+    <Layout title={`Summary of the order ${id}`} description="Summary of the order">
+      <Typography variant="h1" component="h1">
+        Order #{id?.slice(0, 10)}
+      </Typography>
+
+      <Grid container spacing={2} mt={2}>
+        <Grid item xs={12} sm={7}>
+          <CartList orderItems={order.orderItems} />
+        </Grid>
+
+        <Grid item xs={12} sm={5}>
+          <Card className="summary-card">
+            <CardContent>
+              <Typography variant="h2">Summary ({quantity} products)</Typography>
+
+              <Divider sx={{ my: 1 }} />
+
+              <Box display="flex" justifyContent="space-between" alignItems="center">
+                <Typography variant="subtitle1">Delivery Address</Typography>
+
+                <NextLink href="/checkout/address" passHref>
+                  <Link underline="always">Edit</Link>
+                </NextLink>
+              </Box>
+
+              <Typography>
+                {name} {lastName}
+              </Typography>
+
+              <Typography>{address}</Typography>
+
+              <Typography>{country}</Typography>
+
+              <Typography>{phoneNumber}</Typography>
+
+              <Divider sx={{ my: 1 }} />
+
+              <Box display="flex" justifyContent="end">
+                <NextLink href="/cart" passHref>
+                  <Link underline="always">Edit</Link>
+                </NextLink>
+              </Box>
+
+              <OrderSummary data={{ discount, quantity, subTotal, total }} />
+
+              <Box display="flex" alignItems="center" justifyContent="center" mt={3}>
+                {isPaid ? (
+                  <Chip
+                    sx={{ mt: '6px', px: '50px' }}
+                    label="Paid"
+                    variant="outlined"
+                    color="success"
+                    icon={<CreditCardOutlined />}
+                  />
+                ) : (
+                  <Chip
+                    sx={{ mt: '6px', px: '50px' }}
+                    label="Payment Pending"
+                    variant="outlined"
+                    color="error"
+                    icon={<CreditCardOffOutlined />}
+                  />
+                )}
+              </Box>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </Layout>
+  );
+};
+
+export default OrderPage;
+
+export const getServerSideProps: GetServerSideProps<OrderPageProps> = async ({ query, req }) => {
+  try {
+    const { id } = query;
+    const session = await getSession({ req });
+
+    const order = await getOrderByID(id?.toString() || '');
+
+    if (!session || (session?.user as unknown as { _id: string })?._id !== order.user)
+      return {
+        redirect: {
+          destination: '/',
+          permanent: false,
+        },
+      };
+
+    return { props: { order } };
+  } catch (error) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
+};
